4c4242ff2d913c9aae339f04c8f882342469c49f
Beatriz Salles <beatrizsallesss@gmail.com>
2021-11-16 22:34:13 -0300

    chore: Add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -18,28 +18,51 @@ class MyApp extends StatelessWidget {
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
+  void decrement() {
+    print('Decrement');
+  }
+
+  void increment() {
+    print('Increment');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Pode entrar!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700),
+        backgroundColor: Colors.blue,
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text(
+            'Pode entrar!',
+            style: TextStyle(
+                fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
+          ),
+          const Text(
+            '0',
+            style: TextStyle(
+              fontSize: 100,
+              color: Colors.white,
             ),
-            Text(
-              '0',
-              style: TextStyle(
-                fontSize: 100,
-                color: Colors.white,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement,
+                child: const Text('Saiu',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    )),
               ),
-            )
-          ],
-        ));
+              TextButton(
+                  onPressed: increment,
+                  child: const Text('Entrou',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      )))
+            ],
+          )
+        ]));
   }
 }
