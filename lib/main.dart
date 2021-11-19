890e5a6cd70b23ed00ec75432e378c69d6825b27
Beatriz Salles <beatrizsallesss@gmail.com>
2021-11-19 13:12:57 -0300

    chore: Add counter functions

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,38 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
 
   void decrement() {
-    print('Decrement');
+    setState(() {
+      count--;
+    });
   }
 
   void increment() {
-    print('Increment');
+    setState(() {
+      count++;
+    });
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,57 +46,49 @@ class Homepage extends StatelessWidget {
       backgroundColor: Colors.blue,
       body: Container(
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            stops: [0.1, 0.3, 0.7, 1],
-            colors: [Colors.green, Colors.blue, Colors.orange, Colors.pink]
-          )
-        ),
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [
+              0.1,
+              0.3,
+              0.7,
+              1
+            ],
+                colors: [
+              Colors.green,
+              Colors.blue,
+              Colors.orange,
+              Colors.pink
+            ])),
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode entrar!',
-            style: TextStyle(
-                fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          const Text(
-            '0',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Pode entrar!',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700),
             ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: const Size(100, 100),
-                    primary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    )),
-                child: const Text('Saiu',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                    )),
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              '$count',
+              style: const TextStyle(
+                fontSize: 100,
+                color: Colors.white,
               ),
-              const SizedBox(
-                width: 32,
-              ),
-              TextButton(
-                  onPressed: increment,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
                   style: TextButton.styleFrom(
                       backgroundColor: Colors.white,
                       fixedSize: const Size(100, 100),
@@ -91,16 +96,34 @@ class Homepage extends StatelessWidget {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(24),
                       )),
-                  child: const Text('Entrou',
+                  child: const Text('Saiu',
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 16,
-                      )))
-            ],
-          )
-        ],
+                      )),
+                ),
+                const SizedBox(
+                  width: 32,
+                ),
+                TextButton(
+                    onPressed: increment,
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: const Size(100, 100),
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        )),
+                    child: const Text('Entrou',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        )))
+              ],
+            )
+          ],
+        ),
       ),
-    
-      ));
+    );
   }
 }
